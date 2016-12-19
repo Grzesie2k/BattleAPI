6649cc01e411b98176a7710bdca4572e271a474f
Grzesie2k <grzesie.k@o2.pl>
2016-12-19 11:24:38 +0100

    Invoke all methods in tests

--- tests/Game/BF3/SoldierTest.php
+++ tests/Game/BF3/SoldierTest.php
@@ -1,42 +1,55 @@
-<?php namespace BattleAPI\Game\BF3;
+<?php namespace BattleAPI\Tests\Game\BF3;
 
+use BattleAPI\Game\BF3\Soldier;
+use BattleAPI\Game\Platform;
+use BattleAPI\Response\Response;
 use PHPUnit\Framework\TestCase;
 
 class SoldierTest extends TestCase
 {
+    /** @var Soldier */
+    protected $soldier;
 
     public function setUp()
     {
-
+        $platform = new Platform(Platform::PC);
+        $soldierId = '320812621';
+        $this->soldier = new Soldier($soldierId, $platform);
     }
 
     public function testGetOverview()
     {
-        // @TODO Add testGetOverview
+        $soldierOverview = $this->soldier->getOverview();
+        $this->assertInstanceOf(Response::class, $soldierOverview);
     }
 
     public function testGetWeapons()
     {
-        // @TODO Add testGetWeapons
+       $soldierWeapons = $this->soldier->getWeapons();
+       $this->assertInstanceOf(Response::class, $soldierWeapons);
     }
 
     public function testGetVehicles()
     {
-        // @TODO Add testGetVehicles
+        $soldierVehicles = $this->soldier->getVehicles();
+        $this->assertInstanceOf(Response::class, $soldierVehicles);
     }
 
     public function testGetAwards()
     {
-        // @TODO Add testGetVehicles
+        $soldierAwards = $this->soldier->getAwards();
+        $this->assertInstanceOf(Response::class, $soldierAwards);
     }
 
     public function testGetMissions()
     {
-        // @TODO Add testGetMissions
+        $soldierMissions = $this->soldier->getMissions();
+        $this->assertInstanceOf(Response::class, $soldierMissions);
     }
 
     public function testGetDogTags()
     {
-        // @TODO Add testGetDogTags
+        $soldierDogTags = $this->soldier->getDogTags();
+        $this->assertInstanceOf(Response::class, $soldierDogTags);
     }
 }
